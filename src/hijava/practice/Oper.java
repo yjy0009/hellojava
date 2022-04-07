1848f20873ceb0d0215d7ca10f9b9451f20c6196
jyyoon <angels7621@gmail.com>
2022-04-07 10:04:51 +0900

    oper

---
+++ src/hijava/practice/Oper.java
@@ -0,0 +1,16 @@
+package hijava.practice;
+
+public class Oper
+{
+    public static void main(String[] args)
+    {
+        int i = 0;
+        System.out.println(i);
+
+        i++;//i = i +1 ;
+//        i--;//i = i -1 ;
+
+        int j = i++;
+        System.out.println(i++ + ", j= " + j);
+    }
+}
